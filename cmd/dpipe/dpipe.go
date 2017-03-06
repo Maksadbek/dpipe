eaa1158e0a4fb194f051599ed075e2bada181c6d
Maksadbek <m.akxmedov@bitbucket.team>
2017-03-07 01:38:15 +0300

    read config and print list of loaded inputs, outputs, filters and aggregators

--- cmd/dpipe/dpipe.go
+++ cmd/dpipe/dpipe.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"log"
+
+	"github.com/maksadbek/dpipe/config"
+)
+
+var (
+	configFile = flag.String("config", "config", "config file path")
+)
 
 func main() {
-	fmt.Println("dpipe")
+	log.Println("dpipe")
+
+	flag.Parse()
+
+	// load config file
+	conf, err := config.New(*configFile)
+	if err != nil {
+		log.Fatalf("failed to load config file: %v", err)
+	}
+
+	// log loaded inputs, outpus, filters and aggregators
+	log.Printf("loaded inputs: %+v", config.GetAllKeys(conf.Inputs()))
+	log.Printf("loaded outputs: %+v", config.GetAllKeys(conf.Outputs()))
+	log.Printf("loaded filters: %+v", config.GetAllKeys(conf.Filters()))
+	log.Printf("loaded aggregators: %+v", config.GetAllKeys(conf.Aggregators()))
+
 }
